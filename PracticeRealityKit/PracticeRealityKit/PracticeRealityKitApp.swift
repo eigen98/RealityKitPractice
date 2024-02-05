13fc0c4da1fc54cf4e70221841d0505f6142c430
KoJeongmin <terrantrace@gmail.com>
2024-02-05 09:44:29 +0900

    프로젝트 생성

---
+++ PracticeRealityKit/PracticeRealityKit/PracticeRealityKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeRealityKitApp.swift
+//  PracticeRealityKit
+//
+//  Created by Jeongmin Ko on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeRealityKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
